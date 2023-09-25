f523971541372839cfd200966c6f027c8ce54c6d
Gianluca-V <vespe.gianluca@gmail.com>
2023-09-25 16:08:05 -0300

    added dialog modal on wilderArtesanal

--- js/wildeArtesanal.js
+++ js/wildeArtesanal.js
@@ -44,6 +44,10 @@ function generateProductCard(product) {
     const productBuyBtn = document.createElement("button");
     productBuyBtn.textContent = "Comprar";
     productBuyBtn.classList.add("product__buy");
+    productBuyBtn.addEventListener("click",()=>{
+        openProductModal(product);
+        document.querySelector(".product-modal").showModal();
+    });
 
     const productData = document.createElement("div");
     productData.classList.add("product__data");
@@ -57,6 +61,26 @@ function generateProductCard(product) {
     //productCard.appendChild(productDescription);
 
     return productCard;
+}
+
+openProductModal = (product)=>{
+    // Create elements for product details (e.g., name, price, image, description)
+    const productName = document.querySelector(".product-data__title");
+    productName.textContent = product.Name;
+
+    const productPrice = document.querySelector(".product-data__price");
+    productPrice.textContent = `$${product.Price}`;
+
+    const productImage = document.querySelector(".product-modal__img");
+    productImage.src = product.Image != ''? product.Image : "assets/default-img.png";
+    productImage.alt = product.Name;
+
+    const productDescription = document.querySelector(".product-data__description");
+    productDescription.textContent = product.Description;
+
+    const productBuyBtn = document.querySelector("product-modal__buy");
+    //productBuyBtn.addEventListener("click",openProductModal);
+
 }
 
 // Function to display products in the product grid
