9b1bbdb2f98ceccf12d409d6050efc58fcf1fbcc
Gianluca-V <vespe.gianluca@gmail.com>
2023-10-17 00:06:36 -0300

    fixed responsive on news

--- js/map.js
+++
@@ -1,8 +0,0 @@
-function toggleMap() {
-    var mapFrame = document.getElementById('map_frame');
-    if (mapFrame.style.display === 'none' || mapFrame.style.display === '') {
-        mapFrame.style.display = 'block'; // Mostrar el mapa
-    } else {
-        mapFrame.style.display = 'none'; // Ocultar el mapa
-    }
-}
