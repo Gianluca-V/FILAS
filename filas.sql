1edfa67ec3d8d93341cefeffd36a6ec961c7b3a8
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-10-24 14:05:15 -0300

    added family table

--- filas.sql
+++ filas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2023 a las 07:05:46
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 24-10-2023 a las 19:03:39
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,13 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `family`
+--
+
+CREATE TABLE `family` (
+  `ID` int(11) NOT NULL,
+  `Image` text DEFAULT NULL,
+  `Body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `gallery`
 --
 
 CREATE TABLE `gallery` (
   `ID` int(11) NOT NULL,
   `Image` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `gallery`
@@ -52,7 +64,7 @@ INSERT INTO `gallery` (`ID`, `Image`) VALUES
 CREATE TABLE `mails` (
   `ID` int(11) NOT NULL,
   `Mail` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -65,7 +77,18 @@ CREATE TABLE `news` (
   `Title` varchar(99) NOT NULL,
   `Body` text DEFAULT NULL,
   `Image` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `news`
+--
+
+INSERT INTO `news` (`ID`, `Title`, `Body`, `Image`) VALUES
+(1, 'Noticia de prueba 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nisl sed justo cursus finibus. Vivamus quis suscipit turpis. Fusce eu urna magna. Nulla sit amet nunc eget ligula luctus tristique non ornare risus. Quisque nec nulla at massa imperdiet accumsan vitae nec augue. Nunc et nisl justo. Nam venenatis odio sed sapien laoreet, nec lobortis tellus volutpat. Aliquam erat tortor, faucibus sit amet lectus sit amet, tristique mollis magna. Aliquam sollicitudin quis nibh sed dictum.Sed in tempus nisi. Nam in magna pharetra, tempus ante non, tristique nulla. Vivamus consequat lectus eu nulla tristique ultricies. In a sapien vitae nibh accumsan aliquet. Etiam pharetra nisi sit amet consectetur laoreet. Suspendisse id tempor nunc. Vivamus pretium posuere nunc id malesuada. Quisque auctor ut nibh id sollicitudin. In posuere nunc nec imperdiet malesuada. Ut dignissim purus id venenatis porta. In hendrerit purus massa, non tristique eros fermentum nec. Curabitur placerat id ligula sed pharetra. Quisque ac felis nisi. Maecenas gravida cursus placerat. In nisl urna, scelerisque et leo vel, dictum pharetra velit.Nam augue est, elementum eu mauris sit amet, euismod lobortis nunc. Sed tincidunt augue at dolor bibendum, id euismod eros ullamcorper. Morbi elementum libero fringilla lectus volutpat, et rutrum risus blandit. Integer vehicula mi non odio egestas rutrum. Donec iaculis ante ut tincidunt mattis. Praesent sagittis at enim sed auctor. Nulla facilisi. Duis vitae risus sit amet urna convallis cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vitae sodales ex, ut semper magna. Aenean rhoncus dignissim ligula, vitae tincidunt nunc vestibulum eget. Morbi gravida diam sem, congue ultrices ipsum interdum at. Aliquam pellentesque eros sed sapien rhoncus, sed varius ante cursus.Phasellus euismod bibendum lorem at rhoncus. In quis ipsum congue, facilisis purus tincidunt, efficitur felis. Morbi eu laoreet massa. Sed nec metus mauris. Ut dictum sed turpis vel lobortis. Integer facilisis odio vel interdum cursus. Pellentesque accumsan purus non lectus tempus semper. Duis libero sapien, sagittis at scelerisque eu, aliquet dapibus metus. Vestibulum sit amet augue vel nisi tincidunt dapibus. Sed nunc arcu, rutrum in dictum id, euismod in nibh. Curabitur sit amet tempus purus, quis faucibus justo. Mauris et lacus pellentesque libero viverra dignissim. Morbi mattis nisl eu enim placerat, ut consectetur sem sodales. Cras molestie, elit id maximus viverra, magna ex fermentum neque, vehicula pretium nulla erat vel nibh.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras consequat nibh sit amet auctor bibendum. Nunc pharetra interdum metus, id pretium erat efficitur sit amet. Aliquam erat volutpat. Aliquam ac iaculis arcu. Suspendisse maximus lacinia mi, ac vehicula odio malesuada a. Aliquam vitae porttitor lacus. Nulla facilisi. Nulla facilisi. Mauris finibus tortor quam, nec accumsan augue rhoncus euismod.', 'https://www.refugeesrespond.org/dadaabwikimedia/images/archive/a/a9/20201124034818%21Example.jpg'),
+(2, 'Noticia de prueba 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nisl sed justo cursus finibus. Vivamus quis suscipit turpis. Fusce eu urna magna. Nulla sit amet nunc eget ligula luctus tristique non ornare risus. Quisque nec nulla at massa imperdiet accumsan vitae nec augue. Nunc et nisl justo. Nam venenatis odio sed sapien laoreet, nec lobortis tellus volutpat. Aliquam erat tortor, faucibus sit amet lectus sit amet, tristique mollis magna. Aliquam sollicitudin quis nibh sed dictum.Sed in tempus nisi. Nam in magna pharetra, tempus ante non, tristique nulla. Vivamus consequat lectus eu nulla tristique ultricies. In a sapien vitae nibh accumsan aliquet. Etiam pharetra nisi sit amet consectetur laoreet. Suspendisse id tempor nunc. Vivamus pretium posuere nunc id malesuada. Quisque auctor ut nibh id sollicitudin. In posuere nunc nec imperdiet malesuada. Ut dignissim purus id venenatis porta. In hendrerit purus massa, non tristique eros fermentum nec. Curabitur placerat id ligula sed pharetra. Quisque ac felis nisi. Maecenas gravida cursus placerat. In nisl urna, scelerisque et leo vel, dictum pharetra velit.Nam augue est, elementum eu mauris sit amet, euismod lobortis nunc. Sed tincidunt augue at dolor bibendum, id euismod eros ullamcorper. Morbi elementum libero fringilla lectus volutpat, et rutrum risus blandit. Integer vehicula mi non odio egestas rutrum. Donec iaculis ante ut tincidunt mattis. Praesent sagittis at enim sed auctor. Nulla facilisi. Duis vitae risus sit amet urna convallis cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vitae sodales ex, ut semper magna. Aenean rhoncus dignissim ligula, vitae tincidunt nunc vestibulum eget. Morbi gravida diam sem, congue ultrices ipsum interdum at. Aliquam pellentesque eros sed sapien rhoncus, sed varius ante cursus.Phasellus euismod bibendum lorem at rhoncus. In quis ipsum congue, facilisis purus tincidunt, efficitur felis. Morbi eu laoreet massa. Sed nec metus mauris. Ut dictum sed turpis vel lobortis. Integer facilisis odio vel interdum cursus. Pellentesque accumsan purus non lectus tempus semper. Duis libero sapien, sagittis at scelerisque eu, aliquet dapibus metus. Vestibulum sit amet augue vel nisi tincidunt dapibus. Sed nunc arcu, rutrum in dictum id, euismod in nibh. Curabitur sit amet tempus purus, quis faucibus justo. Mauris et lacus pellentesque libero viverra dignissim. Morbi mattis nisl eu enim placerat, ut consectetur sem sodales. Cras molestie, elit id maximus viverra, magna ex fermentum neque, vehicula pretium nulla erat vel nibh.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras consequat nibh sit amet auctor bibendum. Nunc pharetra interdum metus, id pretium erat efficitur sit amet. Aliquam erat volutpat. Aliquam ac iaculis arcu. Suspendisse maximus lacinia mi, ac vehicula odio malesuada a. Aliquam vitae porttitor lacus. Nulla facilisi. Nulla facilisi. Mauris finibus tortor quam, nec accumsan augue rhoncus euismod.', 'https://www.refugeesrespond.org/dadaabwikimedia/images/archive/a/a9/20201124034818%21Example.jpg'),
+(3, 'Noticia de prueba 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nisl sed justo cursus finibus. Vivamus quis suscipit turpis. Fusce eu urna magna. Nulla sit amet nunc eget ligula luctus tristique non ornare risus. Quisque nec nulla at massa imperdiet accumsan vitae nec augue. Nunc et nisl justo. Nam venenatis odio sed sapien laoreet, nec lobortis tellus volutpat. Aliquam erat tortor, faucibus sit amet lectus sit amet, tristique mollis magna. Aliquam sollicitudin quis nibh sed dictum.Sed in tempus nisi. Nam in magna pharetra, tempus ante non, tristique nulla. Vivamus consequat lectus eu nulla tristique ultricies. In a sapien vitae nibh accumsan aliquet. Etiam pharetra nisi sit amet consectetur laoreet. Suspendisse id tempor nunc. Vivamus pretium posuere nunc id malesuada. Quisque auctor ut nibh id sollicitudin. In posuere nunc nec imperdiet malesuada. Ut dignissim purus id venenatis porta. In hendrerit purus massa, non tristique eros fermentum nec. Curabitur placerat id ligula sed pharetra. Quisque ac felis nisi. Maecenas gravida cursus placerat. In nisl urna, scelerisque et leo vel, dictum pharetra velit.Nam augue est, elementum eu mauris sit amet, euismod lobortis nunc. Sed tincidunt augue at dolor bibendum, id euismod eros ullamcorper. Morbi elementum libero fringilla lectus volutpat, et rutrum risus blandit. Integer vehicula mi non odio egestas rutrum. Donec iaculis ante ut tincidunt mattis. Praesent sagittis at enim sed auctor. Nulla facilisi. Duis vitae risus sit amet urna convallis cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vitae sodales ex, ut semper magna. Aenean rhoncus dignissim ligula, vitae tincidunt nunc vestibulum eget. Morbi gravida diam sem, congue ultrices ipsum interdum at. Aliquam pellentesque eros sed sapien rhoncus, sed varius ante cursus.Phasellus euismod bibendum lorem at rhoncus. In quis ipsum congue, facilisis purus tincidunt, efficitur felis. Morbi eu laoreet massa. Sed nec metus mauris. Ut dictum sed turpis vel lobortis. Integer facilisis odio vel interdum cursus. Pellentesque accumsan purus non lectus tempus semper. Duis libero sapien, sagittis at scelerisque eu, aliquet dapibus metus. Vestibulum sit amet augue vel nisi tincidunt dapibus. Sed nunc arcu, rutrum in dictum id, euismod in nibh. Curabitur sit amet tempus purus, quis faucibus justo. Mauris et lacus pellentesque libero viverra dignissim. Morbi mattis nisl eu enim placerat, ut consectetur sem sodales. Cras molestie, elit id maximus viverra, magna ex fermentum neque, vehicula pretium nulla erat vel nibh.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras consequat nibh sit amet auctor bibendum. Nunc pharetra interdum metus, id pretium erat efficitur sit amet. Aliquam erat volutpat. Aliquam ac iaculis arcu. Suspendisse maximus lacinia mi, ac vehicula odio malesuada a. Aliquam vitae porttitor lacus. Nulla facilisi. Nulla facilisi. Mauris finibus tortor quam, nec accumsan augue rhoncus euismod.', 'https://www.refugeesrespond.org/dadaabwikimedia/images/archive/a/a9/20201124034818%21Example.jpg'),
+(4, 'Noticia de prueba 4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nisl sed justo cursus finibus. Vivamus quis suscipit turpis. Fusce eu urna magna. Nulla sit amet nunc eget ligula luctus tristique non ornare risus. Quisque nec nulla at massa imperdiet accumsan vitae nec augue. Nunc et nisl justo. Nam venenatis odio sed sapien laoreet, nec lobortis tellus volutpat. Aliquam erat tortor, faucibus sit amet lectus sit amet, tristique mollis magna. Aliquam sollicitudin quis nibh sed dictum.Sed in tempus nisi. Nam in magna pharetra, tempus ante non, tristique nulla. Vivamus consequat lectus eu nulla tristique ultricies. In a sapien vitae nibh accumsan aliquet. Etiam pharetra nisi sit amet consectetur laoreet. Suspendisse id tempor nunc. Vivamus pretium posuere nunc id malesuada. Quisque auctor ut nibh id sollicitudin. In posuere nunc nec imperdiet malesuada. Ut dignissim purus id venenatis porta. In hendrerit purus massa, non tristique eros fermentum nec. Curabitur placerat id ligula sed pharetra. Quisque ac felis nisi. Maecenas gravida cursus placerat. In nisl urna, scelerisque et leo vel, dictum pharetra velit.Nam augue est, elementum eu mauris sit amet, euismod lobortis nunc. Sed tincidunt augue at dolor bibendum, id euismod eros ullamcorper. Morbi elementum libero fringilla lectus volutpat, et rutrum risus blandit. Integer vehicula mi non odio egestas rutrum. Donec iaculis ante ut tincidunt mattis. Praesent sagittis at enim sed auctor. Nulla facilisi. Duis vitae risus sit amet urna convallis cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vitae sodales ex, ut semper magna. Aenean rhoncus dignissim ligula, vitae tincidunt nunc vestibulum eget. Morbi gravida diam sem, congue ultrices ipsum interdum at. Aliquam pellentesque eros sed sapien rhoncus, sed varius ante cursus.Phasellus euismod bibendum lorem at rhoncus. In quis ipsum congue, facilisis purus tincidunt, efficitur felis. Morbi eu laoreet massa. Sed nec metus mauris. Ut dictum sed turpis vel lobortis. Integer facilisis odio vel interdum cursus. Pellentesque accumsan purus non lectus tempus semper. Duis libero sapien, sagittis at scelerisque eu, aliquet dapibus metus. Vestibulum sit amet augue vel nisi tincidunt dapibus. Sed nunc arcu, rutrum in dictum id, euismod in nibh. Curabitur sit amet tempus purus, quis faucibus justo. Mauris et lacus pellentesque libero viverra dignissim. Morbi mattis nisl eu enim placerat, ut consectetur sem sodales. Cras molestie, elit id maximus viverra, magna ex fermentum neque, vehicula pretium nulla erat vel nibh.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras consequat nibh sit amet auctor bibendum. Nunc pharetra interdum metus, id pretium erat efficitur sit amet. Aliquam erat volutpat. Aliquam ac iaculis arcu. Suspendisse maximus lacinia mi, ac vehicula odio malesuada a. Aliquam vitae porttitor lacus. Nulla facilisi. Nulla facilisi. Mauris finibus tortor quam, nec accumsan augue rhoncus euismod.', 'https://www.refugeesrespond.org/dadaabwikimedia/images/archive/a/a9/20201124034818%21Example.jpg'),
+(5, 'Noticia de prueba 5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nisl sed justo cursus finibus. Vivamus quis suscipit turpis. Fusce eu urna magna. Nulla sit amet nunc eget ligula luctus tristique non ornare risus. Quisque nec nulla at massa imperdiet accumsan vitae nec augue. Nunc et nisl justo. Nam venenatis odio sed sapien laoreet, nec lobortis tellus volutpat. Aliquam erat tortor, faucibus sit amet lectus sit amet, tristique mollis magna. Aliquam sollicitudin quis nibh sed dictum.Sed in tempus nisi. Nam in magna pharetra, tempus ante non, tristique nulla. Vivamus consequat lectus eu nulla tristique ultricies. In a sapien vitae nibh accumsan aliquet. Etiam pharetra nisi sit amet consectetur laoreet. Suspendisse id tempor nunc. Vivamus pretium posuere nunc id malesuada. Quisque auctor ut nibh id sollicitudin. In posuere nunc nec imperdiet malesuada. Ut dignissim purus id venenatis porta. In hendrerit purus massa, non tristique eros fermentum nec. Curabitur placerat id ligula sed pharetra. Quisque ac felis nisi. Maecenas gravida cursus placerat. In nisl urna, scelerisque et leo vel, dictum pharetra velit.Nam augue est, elementum eu mauris sit amet, euismod lobortis nunc. Sed tincidunt augue at dolor bibendum, id euismod eros ullamcorper. Morbi elementum libero fringilla lectus volutpat, et rutrum risus blandit. Integer vehicula mi non odio egestas rutrum. Donec iaculis ante ut tincidunt mattis. Praesent sagittis at enim sed auctor. Nulla facilisi. Duis vitae risus sit amet urna convallis cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vitae sodales ex, ut semper magna. Aenean rhoncus dignissim ligula, vitae tincidunt nunc vestibulum eget. Morbi gravida diam sem, congue ultrices ipsum interdum at. Aliquam pellentesque eros sed sapien rhoncus, sed varius ante cursus.Phasellus euismod bibendum lorem at rhoncus. In quis ipsum congue, facilisis purus tincidunt, efficitur felis. Morbi eu laoreet massa. Sed nec metus mauris. Ut dictum sed turpis vel lobortis. Integer facilisis odio vel interdum cursus. Pellentesque accumsan purus non lectus tempus semper. Duis libero sapien, sagittis at scelerisque eu, aliquet dapibus metus. Vestibulum sit amet augue vel nisi tincidunt dapibus. Sed nunc arcu, rutrum in dictum id, euismod in nibh. Curabitur sit amet tempus purus, quis faucibus justo. Mauris et lacus pellentesque libero viverra dignissim. Morbi mattis nisl eu enim placerat, ut consectetur sem sodales. Cras molestie, elit id maximus viverra, magna ex fermentum neque, vehicula pretium nulla erat vel nibh.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras consequat nibh sit amet auctor bibendum. Nunc pharetra interdum metus, id pretium erat efficitur sit amet. Aliquam erat volutpat. Aliquam ac iaculis arcu. Suspendisse maximus lacinia mi, ac vehicula odio malesuada a. Aliquam vitae porttitor lacus. Nulla facilisi. Nulla facilisi. Mauris finibus tortor quam, nec accumsan augue rhoncus euismod.', 'https://www.refugeesrespond.org/dadaabwikimedia/images/archive/a/a9/20201124034818%21Example.jpg');
 
 -- --------------------------------------------------------
 
@@ -80,7 +103,7 @@ CREATE TABLE `products` (
   `Stock` int(11) NOT NULL,
   `Image` text DEFAULT NULL,
   `Description` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `products`
@@ -108,6 +131,12 @@ INSERT INTO `products` (`ID`, `Name`, `Price`, `Stock`, `Image`, `Description`) 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `family`
+--
+ALTER TABLE `family`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indices de la tabla `gallery`
@@ -138,6 +167,12 @@ ALTER TABLE `products`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `family`
+--
+ALTER TABLE `family`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `gallery`
 --
 ALTER TABLE `gallery`
@@ -153,7 +188,7 @@ ALTER TABLE `mails`
 -- AUTO_INCREMENT de la tabla `news`
 --
 ALTER TABLE `news`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
