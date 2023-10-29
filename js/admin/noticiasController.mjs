95e15f38a9dfd09707cae73a0ebfba86bb617e5d
Gianluca-V <vespe.gianluca@gmail.com>
2023-10-29 00:07:36 -0300

    created admin table for news

--- js/admin/noticiasController.mjs
+++ js/admin/noticiasController.mjs
@@ -1,10 +1,9 @@
 import { API } from "../API.mjs";
 // Function to fetch and display news data
 async function PopulateTable() {
-  const newsTableBody = document.querySelector(".news-table__body");
-
   const newsData = await API.News.GetAll().catch((e) => console.error(e));
 
+  const newsTableBody = document.querySelector(".table__body");
   // Clear existing table rows
   newsTableBody.innerHTML = "";
 
@@ -14,17 +13,20 @@ async function PopulateTable() {
 
     // Add data cells
     row.innerHTML = `
-      <td class="news-table__cell">${news.ID}</td>
-      <td class="news-table__cell">${news.Title}</td>
-      <td class="news-table__cell">${news.Body}</td>
-      <td class="news-table__cell">
-          <img class="news-table__image" src="${news.Image}" alt="${news.Title}">
+      <td class="table__cell table__cell--news" data-cell="#">${news.ID}</td>
+      <td class="table__cell table__cell--news" data-cell="Titulo">${news.Title}</td>
+      <td class="table__cell table__cell--news" data-cell="Cuerpo">${news.Body}</td>
+      <td class="table__cell table__cell--news" data-cell="Imagen">
+          <img class="table__image" src="${news.Image}" alt="${news.Title}">
       </td>
-      <td class="news-table__cell">
-          <button class="news-table__button news-table__button--edit" data-id="${news.id}">Edit</button>
-          <button class="news-table__button news-table__button--delete" data-id="${news.id}">Delete</button>
+      <td class="table__cell table__cell--actions">
+          <button class="table__button table__button--edit" data-id="${news.id}"><svg class="table__svg table__svg--edit" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></button>
+          <button class="table__button table__button--delete" data-id="${news.id}"><svg class="table__svg table__svg--delete" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
       </td>
   `;
   });
 }
 PopulateTable();
+window.addEventListener("hashchange", ()=>{
+  if(window.location.hash.slice(1) === "noticias") PopulateTable();
+});
--- js/adminRouter.mjs
+++ js/adminRouter.mjs
@@ -1,45 +1,39 @@
- window.addEventListener("hashchange", loadContent);
+window.addEventListener("hashchange", loadContent);
 
- loadContent();
+loadContent();
 
- async function loadContent() {
-   const mainContent = document.querySelector(".main");
-   const route = window.location.hash.slice(1);
+async function loadContent() {
+  const mainContent = document.querySelector(".main");
+  const route = window.location.hash.slice(1);
 
-   const HTMLRoutes = {
-     "familia": "js/admin/familiaController.html",
-     "organizaciones": "js/admin/organizacionesController.html",
-     "productos": "js/admin/productosController.html",
-     "noticias": "js/admin/noticiasController.html",
-     "galeria": "js/admin/galeriaController.html",
-   };
-   const JSRoutes = {
-    "familia": "js/admin/familiaController.mjs",
-    "organizaciones": "js/admin/organizacionesController.mjs",
-    "productos": "js/admin/productosController.mjs",
-    "noticias": "js/admin/noticiasController.mjs",
-    "galeria": "js/admin/galeriaController.mjs",
+  const Routes = {
+    "familia": ["js/admin/familiaController.html", "js/admin/familiaController.mjs"],
+    "organizaciones": ["js/admin/organizacionesController.html", "js/admin/organizacionesController.mjs"],
+    "productos": ["js/admin/productosController.html", "js/admin/productosController.mjs"],
+    "noticias": ["js/admin/noticiasController.html", "js/admin/noticiasController.mjs"],
+    "galeria": ["js/admin/galeriaController.html", "js/admin/galeriaController.mjs"],
   };
 
-   try {
-     if (HTMLRoutes[route]) {
-       const response = await fetch(HTMLRoutes[route]);
-       if (response.ok) {
-         const htmlContent = await response.text();
-         mainContent.innerHTML = htmlContent;
-         const script = document.createElement("script");
-         script.setAttribute("src",JSRoutes[route]);
-         script.setAttribute("type","module");
-         script.classList.add("section-script");
-         document.body.appendChild(script);
-       } else {
-         throw new Error(`Failed to load ${route} content.`);
-       }
-     } else {
-       mainContent.innerHTML = "404 - Page Not Found";
-     }
-   } catch (error) {
-     console.error(error);
-     mainContent.innerHTML = "Error loading content.";
-   }
- }
+  try {
+    if (Routes[route][0]) {
+      const response = await fetch(Routes[route][0]);
+      if (response.ok) {
+        const htmlContent = await response.text();
+        mainContent.innerHTML = htmlContent;
+        document.querySelector(".section-script")?.remove();
+        const script = document.createElement("script");
+        script.setAttribute("src", Routes[route][1]);
+        script.setAttribute("type", "module");
+        script.classList.add("section-script");
+        document.body.appendChild(script);
+      } else {
+        throw new Error(`Failed to load ${route} content.`);
+      }
+    } else {
+      mainContent.innerHTML = "404 - Page Not Found";
+    }
+  } catch (error) {
+    console.error(error);
+    mainContent.innerHTML = "Error loading content.";
+  }
+}
