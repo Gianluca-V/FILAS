15f3d8d7444d4eb6e03181e089ebe371de529f34
Gianluca-V <vespe.gianluca@gmail.com>
2023-10-30 00:37:05 -0300

    fixed cors issue

--- FilasServer/index.php
+++ FilasServer/index.php
@@ -6,7 +6,7 @@ $db_user = 'root';
 $db_pass = '';
 
 // Server config to avoid CORS
-header("Access-Control-Allow-Origin: http://127.0.0.1:*");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
