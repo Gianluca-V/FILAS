94800bcdb2e7acf7f14ef1d7e7f6ecfdd02a8bdd
IsNorthFace <joseangarher@gmail.com>
2023-10-30 02:46:18 -0300

    add bd img

--- js/family.mjs
+++ js/family.mjs
@@ -5,5 +5,14 @@ API.Family.GetAll().then((data)=>{
 });
 
 function displayFamily(family){
+    const familySection = document.querySelector(".family");
+
+    family.forEach(element => {
+        familySection.innerHTML +=`<div class="family__events-container">
+    <img src="${ element.Image != '' ? element.Image : "assets/default-img.png"}" alt="${element.Image}" loading="lazy">
+    <p class="section--family__text">${element.Body}</p>
+    </div>`;
+    });
+
     
 }
