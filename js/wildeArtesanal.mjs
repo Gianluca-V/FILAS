e51d5a023ceddd89888144c2a37dbaf23624dfdf
Gianluca-V <vespe.gianluca@gmail.com>
2023-11-11 17:11:53 -0300

    fixed bug on wilderArtesanal

--- js/wildeArtesanal.mjs
+++ js/wildeArtesanal.mjs
@@ -58,7 +58,7 @@ function generateProductCard(product) {
 }
 
 function updateCart() {
-    cartProductsContainer = document.querySelector(".cart__products-container")
+    const cartProductsContainer = document.querySelector(".cart__products-container")
     cartProductsContainer.innerHTML = ""
 
     let totalCost = 0;
@@ -74,20 +74,20 @@ function updateCart() {
     document.querySelector(".cart__buy").removeAttribute("disabled")
 
     cart.forEach(product => {
-        cartProduct = document.createElement("div");
+        const cartProduct = document.createElement("div");
         cartProduct.classList.add("cart__product");
 
-        productName = document.createElement("div")
+        const productName = document.createElement("div")
         productName.classList.add("cart__product-name")
         productName.textContent = product.name;
 
-        productQuantityContainer = document.createElement("div")
+        const productQuantityContainer = document.createElement("div")
         productQuantityContainer.classList.add("cart__product-quantity-container");
         productQuantityContainer.innerHTML = `
         X <input type="number" name="product-quantity" id="product-quantity" class="cart__product-quantity" min="1" value="${product.quantity}" placeholder="1" required>
         `
 
-        productQuantityInput = productQuantityContainer.querySelector("input");
+        const productQuantityInput = productQuantityContainer.querySelector("input");
 
         productQuantityInput.addEventListener("input", (e) => {
             product.quantity = e.target.value;
@@ -95,7 +95,7 @@ function updateCart() {
             return;
         })
 
-        productDelete = document.createElement("button");
+        const productDelete = document.createElement("button");
         productDelete.classList.add("cart__product-delete")
         productDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
         productDelete.addEventListener("click", () => {
