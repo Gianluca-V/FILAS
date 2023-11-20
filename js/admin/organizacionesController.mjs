3118f4d4cadf5b9c2688547d08f316f968d3cedd
Gianluca-V <vespe.gianluca@gmail.com>
2023-11-20 00:47:03 -0300

    fixed bug with organizations admin panel

--- js/admin/organizacionesController.mjs
+++ js/admin/organizacionesController.mjs
@@ -13,6 +13,8 @@ async function PopulateTable() {
 
         // Add data cells
         row.innerHTML = `
+      <td class="table__cell table__cell--Organization" data-cell="#">${Organization.ID}</td>
+      <td class="table__cell table__cell--Organization" data-cell="Titulo">${Organization.Description}</td>
       <td class="table__cell table__cell--Organization" data-cell="Cuerpo">${Organization.Body}</td>
       <td class="table__cell table__cell--Organization" data-cell="Imagen">
           <img class="table__image" src="${Organization.Image}" alt="${Organization.Title}">
