a6a74f083c38136ece40f8cc7bc211ec9ffcd224
Gianluca-V <vespe.gianluca@gmail.com>
2023-12-01 00:24:14 -0300

    styled family section

--- js/family.mjs
+++ js/family.mjs
@@ -16,15 +16,15 @@ function displayFamily(family){
         if(element.Category == "Centro de dia"){
             CDcardsContainer.innerHTML+=`
             <div class="family__card">
-                <div class="family__img">${element.Image !== ''? `<img src="" alt="${element.Image}" loading="lazy" class="family__img">` : ''}</div>
+                <div class="family__img">${element.Image !== ''? `<img src=${element.Image}" alt="Imagen de taller" loading="lazy" class="family__img">` : ''}</div>
                 <div class="family__body">${element.Body}</div>
             </div>`
         }
         
         if(element.Category == 'Taller protegido'){
             TPcardsContainer.innerHTML+=`
-            <div class="family__card">
-                <div class="family__img">${element.Image !== ''? `<img src="" alt="${element.Image}" loading="lazy" class="family__img">` : ''}<div>
+            <div class="family__card family__card--TP">
+                <div class="family__img-container family__img-container--TP">${element.Image !== ''? `<img src="${element.Image}" alt="Imagen de taller" loading="lazy" class="family__img family__img--TP">` : ''}<div>
             </div>`
         }
     });
