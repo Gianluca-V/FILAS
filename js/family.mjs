eb4a5122708dbde4caa12ea30b43f45a6d133de6
IsNorthFace <joseangarher@gmail.com>
2023-11-30 22:15:05 -0300

    changes to section Family

--- js/family.mjs
+++ js/family.mjs
@@ -4,15 +4,28 @@ API.Family.GetAll().then((data)=>{
     displayFamily(data);
 });
 
+
 function displayFamily(family){
-    const familySection = document.querySelector(".family");
-
-    family.forEach(element => {
-        familySection.innerHTML +=`<div class="family__events-container">
-    <img src="${ element.Image != '' ? element.Image : "assets/default-img.png"}" alt="${element.Image}" loading="lazy" class="family__img">
-    <p class="family__text">${element.Body}</p>
-    </div>`;
-    });
-
+    const CDcardsContainer = document.querySelector('.family__cards-container--CD')
+    const TPcardsContainer = document.querySelector('.family__cards-container--TP')
     
+    family.forEach(element => {
+
+        console.log(CDcardsContainer)
+
+        if(element.Category == "Centro de dia"){
+            CDcardsContainer.innerHTML+=`
+            <div class="family__card">
+                <div class="family__img">${element.Image !== ''? `<img src="" alt="${element.Image}" loading="lazy" class="family__img">` : ''}</div>
+                <div class="family__body">${element.Body}</div>
+            </div>`
+        }
+        
+        if(element.Category == 'Taller protegido'){
+            TPcardsContainer.innerHTML+=`
+            <div class="family__card">
+                <div class="family__img">${element.Image !== ''? `<img src="" alt="${element.Image}" loading="lazy" class="family__img">` : ''}<div>
+            </div>`
+        }
+    });
 }
